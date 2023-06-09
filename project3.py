48825199555ce229c4b7b9efb9b5273655da5d71
gusibis <gus1ibis@gmail.com>
2023-06-08 18:58:03 -0600

    more changes

--- project3.py
+++ project3.py
@@ -1,23 +1,22 @@
-
 import requests
-import json
 import pandas as pd
-# from pandas_geojson import read_geojson_url #pip install pandas_geojson, decided to get it with requests. 
 import eel 
 import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # This is to get rid of errors in the terminal, nothing else
 import sqlite3
 import csv
 import os
 import traceback
 import io
-import time
+# from pandas_geojson import read_geojson_url #pip install pandas_geojson, decided to get it with requests. 
+# import json   # didn't need json 
+# import time # did not have to use time. 
 
 class Publish():
     def __init__(self):
-        # self.obesity_end_point = 'https://chronicdata.cdc.gov/resource/hn4x-zwk7.json'
+        # self.obesity_end_point = 'https://chronicdata.cdc.gov/resource/hn4x-zwk7.json' # only 1K records
+        # self.obesity_end_point_geojson = 'https://chronicdata.cdc.gov/resource/hn4x-zwk7.geojson' # only 1K records
         self.obesity_end_point = 'https://chronicdata.cdc.gov/api/views/hn4x-zwk7/rows.csv?accessType=DOWNLOAD'
-        self.obesit_end_point_geojson = 'https://chronicdata.cdc.gov/resource/hn4x-zwk7.geojson'
         eel.init('web', allowed_extensions=['.html'])
         self.eel = eel
         
@@ -25,16 +24,16 @@ class NoSelf():
     def __init__(self):
         global publish
         self.obesity_end_point = publish.obesity_end_point
-        self.obesit_end_point_geojson = publish.obesit_end_point_geojson
+        # self.obesity_end_point_geojson = publish.obesit_end_point_geojson # not used only 1K records and only points to the center of the state
 
     def startPage():
         try:
-            eel.start('project3.html', port=0)
+            eel.start('project3.html', port=0) #port 0 finds any available port. 
         except Exception as e:
             print(f"ERROR {e} KEEP IN MIND THAT THIS SCRIPT REQUIRES YOU TO pip install eel")
   
     @eel.expose
-    def updateDatabase():
+    def updateDatabase(): # The DB is updated from the CSV file cleaned when collecting the data. 
         conn = sqlite3.connect('./Resources/obesity.db')
         try:
             conn.execute('''CREATE TABLE obesity_data (YearEnd,LocationDesc,Question,Data_Value,Age_years,GeoLocation)''') 
@@ -43,11 +42,9 @@ class NoSelf():
             eel.updateMessage("Database already exist reading it")
             NoSelf.readDataBase()
             return
-
         try:
-            # Import data from CSV file
             c = conn.cursor()
-            with open('./Resources/obesity_data.csv', 'r') as f:
+            with open('./Resources/obesity_data.csv', 'r') as f: 
                 reader = csv.reader(f)
                 for row in reader:
                     if row[0] == 'yearstart': continue
@@ -91,20 +88,7 @@ class NoSelf():
             tempList.append(d)
             eel.updateMessage(str(row))
             # print(row)
-        eel.updateMessage(" COMPLETED READING DATABASE")
-        # y11 = [x for x in tempList if x['YearEnd'] == '2011']
-        # y12 = [x for x in tempList if x['YearEnd'] == '2012']
-        # y13 = [x for x in tempList if x['YearEnd'] == '2013']
-        # y14 = [x for x in tempList if x['YearEnd'] == '2014']
-        # y15 = [x for x in tempList if x['YearEnd'] == '2015']
-        # y16 = [x for x in tempList if x['YearEnd'] == '2016']
-        # y17 = [x for x in tempList if x['YearEnd'] == '2017']
-        # y18 = [x for x in tempList if x['YearEnd'] == '2018']
-        # y19 = [x for x in tempList if x['YearEnd'] == '2019']
-        # y20 = [x for x in tempList if x['YearEnd'] == '2020']
-        # y21 = [x for x in tempList if x['YearEnd'] == '2021']
-
-
+        eel.updateMessage(" FINISHED READING DATABASE. SELECT YEAR OR STATE")
         return(tempList)
 
     @eel.expose
@@ -123,13 +107,11 @@ class NoSelf():
                 data.append(row)
    
             df = pd.DataFrame.from_records(data)
-            # df = df.dropna()
             df.columns = df.columns.str.replace('[:,@]', '', regex=True)
             df.columns = df.columns.str.replace('[.]', '_', regex=True)
             df = df[['YearEnd', 'LocationDesc', 'Question', 'Data_Value', 'Age(years)', 'GeoLocation']]
             df = df.rename(columns={'Age(years)': 'Age_years'}) #changed this name becuase caused issues with DB. 
-            # print (df.isnull().sum())
-            df_clean = df.dropna() #Do not seem to be nas are maybe blank row. 
+            df_clean = df.dropna() #Does not have NaNs are maybe blank rows only but does not hurt. 
             df_clean = df_clean[df_clean.YearEnd != ''] #Removed blanks them like this
             df_clean = df_clean[df_clean.LocationDesc != '']
             df_clean = df_clean[df_clean.Question != '']
@@ -137,13 +119,13 @@ class NoSelf():
             df_clean = df_clean[df_clean.Age_years != '']
             df_clean = df_clean[df_clean.GeoLocation != '']
             df_clean.to_csv('./Resources/obesity_data.csv', index=False, encoding='utf-8')
-
             eel.updateMessage("")
             d = df_clean.to_json()
             NoSelf.createDataBase(d)
             return(d)
         except Exception as e:
             print(e)
+            eel.updateMessage(e)
 
     def createDataBase(d):
         conn = sqlite3.connect('./Resources/obesity.db')
@@ -156,26 +138,20 @@ class NoSelf():
             return
 
         try:
-            # Import data from CSV file
             c = conn.cursor()
-            with open('./Resources/obesity_data.csv', 'r') as f:
+            with open('./Resources/obesity_data.csv', 'r') as f: # Import data from CSV file created when colecting data to DB
                 reader = csv.reader(f)
                 for row in reader:
                     if row[0] == 'YearEnd': continue
                     clause = 'INSERT INTO obesity_data (YearEnd,LocationDesc,Question,Data_Value,Age_years,GeoLocation)\
                             VALUES(?,?,?,?,?,?)' 
                     c.execute(clause,row)
-            # Query data from table
-            # cursor = conn.execute("SELECT * from obesity_data")
-            # for row in cursor:
-            #     eel.updateMessage(row)
             conn.commit()
             conn.close()
         except Exception as e:
             traceback.print_exc()
             message = "THE DATABASE CODE HAS ISSUES IN PYTHON... FUNCTION 'updateDatabase'  " + str(e)
             eel.updateMessage(message)
-
 
 if __name__ == "__main__":
     if not os.path.exists("./Resources"):
